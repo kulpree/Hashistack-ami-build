b8b79d840b0d16d72ffb05fada782271465031a3
Kulpreet Singh <kulpreet.singh@wnco.com>
2021-06-21 11:47:14 -0700

    fix(): adding required modules

--- packer/nomad-consul-ami/setup_nomad_consul.sh
+++ packer/nomad-consul-ami/setup_nomad_consul.sh
@@ -4,5 +4,5 @@ set -e
 # Environment variables are set by packer
 /tmp/Hashistack-ami-build/modules/install-nomad/install-nomad --version "${NOMAD_VERSION}"
 
-git clone --branch "${CONSUL_MODULE_VERSION}"  https://github.com/hashicorp/terraform-aws-consul.git /tmp/terraform-aws-consul
-/tmp/terraform-aws-consul/modules/install-consul/install-consul --version "${CONSUL_VERSION}"
+# git clone --branch "${CONSUL_MODULE_VERSION}"  https://github.com/hashicorp/terraform-aws-consul.git /tmp/terraform-aws-consul
+/tmp/Hashistack-ami-build/modules/install-consul/install-consul --version "${CONSUL_VERSION}"
